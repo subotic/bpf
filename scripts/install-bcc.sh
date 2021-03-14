bcbff27f7335711d6e28c67f8d4280ccf3d40e87
Ivan Subotic <400790+subotic@users.noreply.github.com>
2021-03-14 19:01:30 +0100

    chore(vagrant): add golang

--- scripts/install-bcc.sh
+++ scripts/install-bcc.sh
@@ -4,7 +4,8 @@ set -eux
 
 # install make
 sudo apt-get update
-sudo apt-get -y install make
+sudo apt-get -y install make wget
+sudo apt-get install -y openjdk-11-jdk-headless
 
 # install python 3.8
 sudo apt-get install -y python3
@@ -13,6 +14,10 @@ sudo apt-get install -y python3-pip
 
 # install bcc
 sudo apt-get install -y bpfcc-tools "linux-headers-$(uname -r)"
+
+# install go
+wget -c https://dl.google.com/go/go1.14.2.linux-amd64.tar.gz -O - | sudo tar -xz -C /usr/local
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.profile
 
 # install bazelisk
 sudo curl -Lo /usr/local/bin/bazel https://github.com/bazelbuild/bazelisk/releases/download/v1.7.5/bazelisk-linux-amd64
